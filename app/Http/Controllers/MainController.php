2b315a04d0180f1b642e0e807fd3843c8e7bb112
Admin <aziz89131983189@gmail.com>
2022-04-02 16:51:02 +0700

    fixed default layout

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,13 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Brands;
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class MainController extends Controller
 {
 
     public function index()
     {
-        return view('index');
+        $brands = Brands::limit(3)->get();
+        $hits =Product::where([
+            ['hit','=','1'],
+            ['status','=','1'],
+        ])->limit(8)->get()->chunk(4);
+//        //$hits = array_chunk($hits,4);
+//        dump($hits);
+
+        return view('index',compact('brands','hits'));
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,47 +2,64 @@
 
 
 @section('content')
-        <!--about-starts-->
-        <div class="about">
-            <div class="container">
-                <div class="about-top grid-1">
-                    <div class="col-md-4 about-left">
-                        <figure class="effect-bubba">
-                            <img class="img-responsive" src="{{asset('assets/images/abt-1.jpg')}}" alt=""/>
-                            <figcaption>
-                                <h2>Nulla maximus nunc</h2>
-                                <p>In sit amet sapien eros Integer dolore magna aliqua</p>
-                            </figcaption>
-                        </figure>
-                    </div>
-                    <div class="clearfix"></div>
-                </div>
-            </div>
-        </div>
-        <!--about-end-->
-        <!--product-starts-->
-        <div class="product">
-            <div class="container">
-                <div class="product-top">
-                    <div class="product-one">
-                        <div class="col-md-3 product-left">
-                            <div class="product-main simpleCart_shelfItem">
-                                <a href="single.html" class="mask"><img class="img-responsive zoom-img" src="{{asset('assets/images/p-1.png')}}" alt="" /></a>
-                                <div class="product-bottom">
-                                    <h3>Smart Watches</h3>
-                                    <p>Explore Now</p>
-                                    <h4><a class="item_add" href="#"><i></i></a> <span class=" item_price">$ 329</span></h4>
-                                </div>
-                                <div class="srch">
-                                    <span>-50%</span>
-                                </div>
+        @if($brands)
+            <!--about-starts-->
+            <div class="about">
+                <div class="container">
+                    <div class="about-top grid-1">
+                        @foreach($brands as $brand)
+                            <div class="col-md-4 about-left">
+                                <figure class="effect-bubba">
+                                    <img class="img-responsive" src="{{ asset("assets/images/{$brand->img}") }}" alt=""/>
+                                    <figcaption>
+                                        <h2>{{ $brand->title }}</h2>
+                                        <p>{{ $brand->description }}</p>
+                                    </figcaption>
+                                </figure>
                             </div>
-                        </div>
+                        @endforeach
                         <div class="clearfix"></div>
                     </div>
-
                 </div>
             </div>
-        </div>
-        <!--product-end-->
+        <!--about-end-->
+        @endif
+        @if ($hits)
+            <!--product-starts-->
+            <div class="product">
+                <div class="container">
+                    <div class="product-top">
+                        @for($i=0;$i<count($hits);$i++)
+                            <div class="product-one">
+                                @foreach($hits[$i] as $hit)
+                                    <div class="col-md-3 product-left">
+                                        <div class="product-main simpleCart_shelfItem">
+                                            <a href="product/{{$hit->alias}}" class="mask"><img class="img-responsive zoom-img" src="{{asset("assets/images/{$hit->img}")}}" alt="" /></a>
+                                            <div class="product-bottom">
+                                                <h3><a href="product/{{ $hit->alias }}">{{ $hit->title }}</a></h3>
+                                                <p>Explore Now</p>
+
+                                                <h4>
+                                                    <a data-id="<{{ $hit->id }}" class="add-to-cart-link" href="cart/add?id={{$hit->id}}"><i></i></a> <span class=" item_price">{{ $hit->price }}</span>
+                                                    @if($hit->old_price)
+                                                        <small><del>{{ $hit->old_price }}</del></small>
+                                                     @endif
+                                                </h4>
+                                            </div>
+                                            @if($hit->old_price)
+                                                <div class="srch">
+                                                    <span>{{'A discount : ' .round((($hit->old_price-$hit->price)/$hit->old_price)*100) }}%</span>
+                                                </div>
+                                            @endif
+                                        </div>
+                                    </div>
+                                @endforeach
+                            <div class="clearfix"></div>
+                        </div>
+                        @endfor
+                    </div>
+                </div>
+            </div>
+            <!--product-end-->
+    @endif
 @endsection
